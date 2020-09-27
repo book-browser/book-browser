f5d3a0e38eca1deabee58593d13051719aff6dbb
tbroadnax <tbroadnax@plan-sys.com>
2020-09-27 11:40:38 -0400

    bb-11: added actuator endpoints. updated register page to display api errors.

--- src/main/ui/src/app/page/register/register.component.ts
+++ src/main/ui/src/app/page/register/register.component.ts
@@ -3,6 +3,8 @@ import { User } from 'src/app/entity/user';
 import { NgForm } from '@angular/forms';
 import { UserService } from 'src/app/service/user.service';
 import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ApiError } from 'src/app/entity/api-error';
 
 @Component({
   selector: 'app-register',
@@ -11,6 +13,7 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent {
   registering = false;
+  errorMessage: string;
 
   constructor(private userService: UserService, private router: Router) { }
 
@@ -20,6 +23,12 @@ export class RegisterComponent {
       this.userService.register(form.value as User)
           .subscribe(() => {
             this.router.navigateByUrl('/register/success');
+          }, (errorResponse: HttpErrorResponse) => {
+            this.registering = false;
+            const error: ApiError = errorResponse.error;
+            if (error.status === 500) {
+              this.errorMessage = 'Something went wrong';
+            }
           });
     }
   }
